39ca6f7e8aee4ba847245074ce0f59e6b6d6defd
ianZzzzzz <zhengyh186@gmail.com>
2020-12-08 23:18:07 +0800

    cube

---
+++ doc/K_version_EDA.py
@@ -0,0 +1,15 @@
+dtypes = {
+    "row_id": "int64",
+    "timestamp": "int64",
+    "user_id": "int32",
+    "content_id": "int16",
+    "content_type_id": "boolean",
+    "task_container_id": "int16",
+    "user_answer": "int8",
+    "answered_correctly": "int8",
+    "prior_question_elapsed_time": "float32", 
+    "prior_question_had_explanation": "int8"
+}
+path = ''
+train = cudf.read_csv(path, dtype=dtypes)
+
