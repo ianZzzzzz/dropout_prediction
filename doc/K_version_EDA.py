dd91ec6e18b13546f419be769ee0d8a02ed618fc
ianZzzzzz <zhengyh186@gmail.com>
2020-12-09 21:53:47 +0800

    kaggle load describe

--- doc/K_version_EDA.py
+++ doc/K_version_EDA.py
@@ -1,7 +1,7 @@
 dtypes = {
-    "row_id": "int64",
+    "enroll_id": "int64",
     "timestamp": "int64",
-    "user_id": "int32",
+    "username": "int32",
     "content_id": "int16",
     "content_type_id": "boolean",
     "task_container_id": "int16",
@@ -13,3 +13,11 @@ dtypes = {
 path = ''
 train = cudf.read_csv(path, dtype=dtypes)
 
+data_type = {
+    'enroll_id':'int64'
+    ,'username':'int64'
+    ,'course_id':'str'
+    ,'session_id':'str'
+    ,'action':'str'
+    ,'object':'str'
+    ,'time':'str'}      
