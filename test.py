cd754fcdcef5cb21f7230003f4549be6c4c459eb
ianZzzzzz <zhengyh186@gmail.com>
2020-12-20 14:09:36 +0800

    too slow ,using numpy to accelerate

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pandas as cudf
 from pandas import DataFrame
-for numpy import ndarray
+from numpy import ndarray
 from datetime import datetime
 from typing import List, Dict 
 import os
@@ -30,32 +30,36 @@ def load_data(path_dict: Dict[str,str],base: str)-> Dict[str,DataFrame]:
                 "Columns: {}".format(len(raw_data[file[:-4]].columns)),'\n')
     print('load finish by ',base)
     return raw_data
-def find_null_set_null(raw_data:DataFrame,base: str,defult: int)->DataFrame:
+def find_null_set_null(raw_dict:Dict[str,DataFrame],base: str,defult: int)->DataFrame:
     print('find_null run')
-    for i in raw_data:    
-        print(i,' :')
-        total = len(raw_data[i])
-        for column in raw_data[i].columns:
-            if raw_data[i][column].isna().sum() != 0:
-                
-                print("{} has: {:,} ,{:.2f}% missing values.".format(column, raw_data[i][column].isna().sum(), 
-                                                                     (raw_data[i][column].isna().sum()/total)*100))
-                raw_data[i][column] = raw_data[i][column].fillna(defult) # set null = -1
-            else : print(column,' ','does not find null')
-        print('\n')
-    return raw_data
+    for i in raw_dict:
+        print(i,' : \n')
+        raw_data = raw_dict[i]
+        for i in raw_data:    
+            print(i,' :')
+            total = len(raw_data[i])
+            for column in raw_data[i].columns:
+                if raw_data[i][column].isna().sum() != 0:
+                    
+                    print("{} has: {:,} ,{:.2f}% missing values.".format(column, raw_data[i][column].isna().sum(), 
+                                                                        (raw_data[i][column].isna().sum()/total)*100))
+                    raw_data[i][column] = raw_data[i][column].fillna(defult) # set null = -1
+                else : print(column,' ','does not find null')
+            print('\n')
+        raw_dict[i] = raw_data
+    return raw_dict
 def print_nunique_in_each_col(df:DataFrame)->None:
     for col in train_log.columns:
         print( 'in ',col,' : ',
             train_log[col].unique(),'\n',
             train_log[col].nunique(),' unique values'
             )
-def preprocess(train_log:DataFrame)-> Dict[str,Dict[int,DataFrame]]:
+def preprocess(log:DataFrame)-> Dict[str,Dict[int,DataFrame]]:
     enroll_dict  = {}
     course_dict  = {}
     student_dict = {}
 
-    for i,v in train_log.iterrows() :
+    for i,v in log.iterrows() :
         # init
         if v['enroll_id'] not in enroll_dict:
             enroll_dict[v['enroll_id']] = cudf.DataFrame()
@@ -66,7 +70,8 @@ def preprocess(train_log:DataFrame)-> Dict[str,Dict[int,DataFrame]]:
         # init end
         enroll_dict[v['enroll_id']]  = enroll_dict[v['enroll_id']].append(
             v.drop(labels =['enroll_id','username','course_id']))
-        
+        # enroll_dict[i] including--> 'action''time''object'
+
         if v.username not in course_dict[v['course_id']].values:
             course_dict[v['course_id']]  = course_dict[v['course_id']].append([v.username])
         if v.course_id not in student_dict[v['username']].values:
@@ -74,49 +79,39 @@ def preprocess(train_log:DataFrame)-> Dict[str,Dict[int,DataFrame]]:
 
     # sort
     for i in enroll_dict:
-        def to_json(df,orient='split'):
-            df_json = df.to_json(orient = orient, force_ascii = False)
-            return json.loads(df_json)
-
         enroll_dict[i] = enroll_dict[i].sort_values(by = ['time'])
+    
+    def filter(enroll_dict)-> Dict[int,DataFrame]:
+        for i in enroll_dict:
+            if len(enroll_dict[i])<5 :
+                del enroll_dict[i]
+        return enroll_dict
+    enroll_dict = filter(enroll_dict)
 
 
     processed_data= {
         'enroll':enroll_dict,
         'course':course_dict,
         'student':student_dict }
+
+    
     return processed_data
+#def time_convert(log:DataFrame)->log:DataFrame:
+#    pass
 def tokenlization(s):
     actions = set()
     # actons.update()
 
     return s
 
-
-''' 
-
-        enroll_dict[i] = enroll_dict[i].to_dict(orient='records') # to_json(enroll_dict[i])
-
-    for 
-    with open("json_file//train_log.json","w") as f:
-        json.dump(enroll_dict,f)
-        print("writed to json")
-    js = cudf.read_json('json_file//train_log.json')
-    for i in js:
-        js[i] = cudf.DataFrame.from_dict(js[i], orient='index')
-    print(js)
-'''
-ACTION_MAP = {
-    1:'load_video'
-}
-def to_vec(dict_enroll : Dict[int , Dataframe] )->DataFrame:
+def to_vec(dict_enroll : Dict[int,DataFrame])->DataFrame:
     def find_gap(i : int)->Dict[str,int]:
         t = {'start':0, 'end':0}
         return t
     def incert_time(df : DataFrame, start : int, end : int )->ndarray:
         log = DataFrame.values
         return log
-    def zip_log(log : ndarray, defult_len : int)->ndarray:
+    def   zip_log(log : ndarray, defult_len : int)->ndarray:
         return log_zip
     for i in dict_enroll :
         start,end = find_gap(i)
@@ -130,20 +125,11 @@ def seriesNN(action_series):
     network = fit(action_series)
     return network_para
 
-for series in to_vec(dict_enroll):
-    para = seriesNN(series)
-    
-
-{
-    1:Dataframe()
-}
-data = []
-
-
-# 
+'''
 path = 'D:\\zyh\\_workspace\\dropout_prediction\\test'
 path_dict = return_file(path)
 df =  find_null_set_null(  load_data(path_dict, base = 'cudf')  ,base ='cudf',defult= -1)
 train_log = df['train_log']
 user_id = df['user_info'].index.unique()
 
+'''
