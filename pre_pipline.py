94c1893b73d3a5f7a2b60d36695a3df78a6334c8
ianZzzzzz <zhengyh186@gmail.com>
2021-01-03 23:51:47 +0800

    convert finish

--- pre_pipline.py
+++ pre_pipline.py
@@ -1,6 +1,19 @@
 '''
     进展 ：
-      读取csv数据对其归类存字典 转换成训练数据 用自编码器编码
+      读取csv数据对其归类存字典 转换成训练数据 
+      特征包含：
+        1 不同action 的组合模式与先后模式
+        2 action之间的间隔时间
+        3 用户主要的操作分布在开课时间的哪一部分
+
+     考虑后决定暂不使用特征3 因为使用完整的序列会使得训练样本太稀疏 导致训练缓慢
+
+     编码：
+        怎么选择合适的编码将22种符号特征编码成向量 
+        要求近似的行为编码的余弦相似度等指标也接近
+
+        onehot显然不合适 选择词嵌入方式训练 
+        AAAI19论文的代码中将行为分成了四类，
 
 '''
 '''程序功能：
@@ -55,6 +68,109 @@ def load(
     if return_mode == 'values':return log.values
 
 @_t
+def convert(
+    log: dict )->dict: 
+    print('dict total len :',len(log))
+    print(' convert running!')
+    
+    import numpy as np
+    
+    def find_start_end(c_id:str)->Dict[int,datetime64]:
+
+        ''' 根据course_id 查询课程的总耗时秒数 以及开始时间并返回
+            函数调用了全局变量C_INFO_NP必须在课程信息被加载后才能运行'''
+        mask = C_INFO_NP[:,1] == c_id
+
+        start = C_INFO_NP[mask][:,2]
+        end   = C_INFO_NP[mask][:,3]
+        #type: object ['2016-11-16 08:00:00']
+        start = str(start)
+        end = str(end)
+        #type: str ['2016-11-16 08:00:00']
+        start = start[2:-2]
+        end = end[2:-2]
+        #type: str '2016-11-16 08:00:00'
+        try:
+            end = np.datetime64(end) 
+            start = np.datetime64(start)
+            seconds_of_gap = int((end - start).item().total_seconds())
+        except:print('ERROR start,end :',start,end)
+        time_info = {
+            'length': seconds_of_gap
+            ,'head' : start}
+        return time_info
+    @_t
+    def time_map(log_np:ndarray)->ndarray:
+        def to_int(x):
+            return int(x)
+        md = map(to_int,log_np[:,1]) 
+        __time = list(md) # time list
+
+        __head = np.min(__time)
+        __tail = np.max(__time)
+        __length = __tail - __head +1
+        action_series = np.zeros((__length,1),dtype='a32')
+
+        for row in log_np:
+            __t = int(row[1]) # time now
+            __location = __t - __head
+            action_series[__location] = row[0]
+        return action_series
+
+    new_dict = {}
+    for e_id ,v in log.items():
+        # 遍历字典 
+        # type(v)==list
+        v = np.array(v)
+        c_id = v[0,2] 
+        # 上一函数为了方便 保留了每一行的course_id 此处只需要一个值
+        _log       = v[:,[0,1]]
+        time_col   = _log[:,1]
+        action_col = _log[:,0] 
+        
+        time_info   = find_start_end(c_id)
+        time_head   = time_info['head']
+        time_length = time_info['length']
+
+        list_time   = np.zeros((len(_log),1),dtype = np.uint32)
+        list_action = np.zeros((len(_log),1),dtype = 'a32') # a32 存32个英文字符
+        
+
+        for row_num in range(len(_log)):
+            ''' 为了对action做进一步编码 保留了action接口 '''
+
+            _row = _log[row_num,:]
+            _time = _row[1]
+            _action = _row[0]
+            try:
+                _time = np.datetime64(_time)
+                _time =  int(
+                    ( _time - time_head ).item().total_seconds() )
+                list_time[row_num] = _time
+
+                list_action[row_num] = _action
+            except:print('ERROR log time [_time] :',_time)
+        rebulid = np.concatenate( (list_action, list_time), axis = 1)
+        rebulid[:,1] = rebulid[:,1].astype('u4')
+
+        '''出于保留 ‘用户主要的操作分布在开课时间的哪一部分’ 这一特征 
+            的目的，将时间转换部分分为两部分写，后期如需重建此特征以上的代码可以不动'''
+
+        action_series = time_map(rebulid)
+        new_dict[e_id] =  action_series 
+        
+    return new_dict
+
+
+
+'''此分类引用自AAAI19那篇论文的代码
+    video_action = ['seek_video','play_video','pause_video','stop_video','load_video']
+    problem_action = ['problem_get','problem_check','problem_save','reset_problem','problem_check_correct', 'problem_check_incorrect']
+    forum_action = ['create_thread','create_comment','delete_thread','delete_comment']
+    click_action = ['click_info','click_courseware','click_about','click_forum','click_progress']
+    close_action = ['close_courseware']
+'''
+@_t
 def cut_nan(
     log: ndarray,
     key_col:list
@@ -77,139 +193,7 @@ def cut_nan(
         log = log[~mask]
     
     return log
-@_t
-def time_convert(
-    log: dict )->ndarray: 
-    print('dict total len :',len(log))
-    print('time convert running!')
-    # 2015-09-25 08:00:00,2016-01-06 08:00:00
-    import numpy as np
-    
-    for k ,v in log.items():
-        # 遍历字典 
-        # type(v)==list
-        v = np.array(v)
-        time_col = v[:,1]
-        action_col = v[:,0] 
-        
-        c_id = v[0,2]
-        def find_start_end(c_id:str)->Dict[int,datetime64]:
-            ''' 根据course_id 查询课程的总耗时秒数 以及开始时间并返回
-                函数调用了全局变量C_INFO_NP必须在课程信息被加载后才能运行'''
-            mask = C_INFO_NP[:,1] == c_id
 
-            start = C_INFO_NP[mask][:,2]
-            end   = C_INFO_NP[mask][:,3]
-            #type: object ['2016-11-16 08:00:00']
-            start = str(start)
-            end = str(end)
-            #type: str ['2016-11-16 08:00:00']
-            start = start[2:-2]
-            end = end[2:-2]
-            #type: str '2016-11-16 08:00:00'
-
-            end = np.datetime64(end) 
-            start = np.datetime64(start)
-            seconds_of_gap = int((end - start).item().total_seconds())
-
-            time_info = {
-                'length': seconds_of_gap
-                ,'head' : start_time}
-            return time_info
-        time_info = find_start_end(c_id)
-        
-        time_head = time_info['head']
-        time_length = time_info['length']
-        
-        
-        action_col
-        time_col
-        time_head
-        time_length
-
-
-        
-    
-        #print('时间转化完成')
-        #3转秒
-            #(1)
-        
-        total_seconds_of_gap = int((real_time - start_time).item().total_seconds())
-        delta = total_seconds_of_gap # / seconds_per_hour
-        delta = int(delta)
-        date_time_array[row] = delta
-
-    date_time_array = np.zeros((len(log),1),dtype=np.int32)
-    # 存储一个月的秒数 需要2^22 存储一年的秒数需要2^25
-    
-    
-
-
-    for row in range(len(log)):
-        if (row%print_batch)==0:print('converted row : ',row)
-        #1拼接
-        if merge == True: 
-            def merge_col(log: ndarray)-> str:
-                
-                date = log[0].replace('/','-')
-                # yyyy-m-d -> yyyy-mm-dd 以适应numpy严格的格式要求
-                if len(date)<10:
-                    if len(date) == 8:
-                        date = date[0:5]+'0'+date[5:7]+'0'+date[7]
-                    else:
-                       # print('date:',date)
-                        if date[6]=='-':
-                            date = date[0:5]+'0'+date[5:]
-                        else:
-                            date = date[0:8]+'0'+date[8]
-                time = log[1]
-                
-                if time[1]==':':time = '0'+time
-                date__time = date+'T'+time
-                return date__time
-            date_time_a16 = merge_col(log[row,date_time_col]) 
-            real_time = date_time_a16
-           # print('real_time:',real_time)
-            # 此处用一个type=a16变量暂存date_time字符串
-            # WARNING 警告 两个字符串相加会因为原先的字符串类型位数不够 导致相加失败 但是不报错
-            # date_time covered the date column
-        else:   
-            real_time = log[row,date_time_col]
-        
-        #2转化
-        
-        real_time = np.datetime64(real_time) 
-        start_time = np.datetime64(start_time)
-        #print('时间转化完成')
-        #3转秒
-            #(1)
-        
-        total_seconds_of_gap = int((real_time - start_time).item().total_seconds())
-        delta = total_seconds_of_gap # / seconds_per_hour
-        delta = int(delta)
-        date_time_array[row] = delta
-        #print('转秒完成')
-       
-    
-    # 合并秒数列和log 清除原来date time 列
-    print(
-        'len date_time_array: ',len(date_time_array)
-        ,'len log:',len(log))
-    new_log = np.concatenate(
-         (log[:,[0,1,2,4]],date_time_array) 
-         ,axis = 1)
-         # enroll_id,username,course_id,actions + seconds
-    
-    print('concat finish!')
-    # 类型转换
-    new_log[:,0] = new_log[:,0].astype('u4')
-    new_log[:,1] = new_log[:,1].astype('u4')
-    #new_log[:,2] = ((new_log[:,2].astype('f2'))*1024).astype('u2')
-    #new_log[:,3] = new_log[:,3].astype('u4')
-    new_log[:,4] = new_log[:,1].astype('u4')
-    print('astype finish')
-
-    return new_log
 @_t
 def to_dict_2(log: ndarray)-> Dict[str,ndarray]: 
     '''优化版 时间复杂度低'''
@@ -232,55 +216,7 @@ def to_dict_2(log: ndarray)-> Dict[str,ndarray]:
         if (i%print_batch)==0:print('already dict : ',i,'row logs')
     
     return log_dict
-@_t
-def time_map(
-    log_dict:dict   
-    )->dict:
-    '''程序功能：
 
-        接收list
-        使用list中的数据列的头尾两行的时间值作为序列起始和终止时间点
-        如果无序 则先排序
-        根据起始和终止时间点以及通过EDA确定的采样频率
-        确定时间序列坐标轴->time_list
-        遍历输入的数据的时间列 
-        将其填充到time_list
-        对于缺失值填充方式
-             置0    '''
-    import numpy as np
-    def mapping(log: ndarray)->ndarray:
-
-        time_col = log[:,2]
-        
-        start = time_col[0]
-        end = time_col[-1]
-        
-        length = int(end)-int(start)+1
-        data_width = 2
-
-        ts = np.zeros((length,data_width),dtype = np.int32)
-
-        for row in log:
-            time = row[2] - start # 时间值 也就是ts中的位置
-            # BUG 逻辑不完备 默认了序列的起始是1 
-            # 但实际序列可能是8到10这样 间距是3
-            # 程序找的索引却是list[8]而不是list[0]
-            
-            ts[time,0] = row[0]
-            ts[time,1] = row[1]
-        
-        return ts
-    
-    ts_dict = {}
-    j = 0
-    for k,v in log_dict.items():
-        v = np.array(v)
-        ts_dict[k] = mapping(v)
-
-        j+=1
-        if (j%10000)==0:print('already map :',j,'area')
-    
-    return ts_dict
 @_t
 def to_json(path,dict_log: Dict[int,ndarray]):
     '''dict[    enroll_id : array.tolist()]-->json_txt
@@ -292,6 +228,7 @@ def to_json(path,dict_log: Dict[int,ndarray]):
 '''
     import json
     json.dump(dict_log,open(path,'w'))
+
 log_path = 'test\\prediction_log\\test_log.csv'
 log_col = ['enroll_id','username','course_id','session_id','action','object','time']
 
@@ -313,10 +250,40 @@ C_INFO_NP = load(
     columns =c_info_col
     )
 
-log_np_convert = dict_time_convert(log_dict)
-ts_dict = time_map(log_dict) 
-to_json(path = 'processed_log.json',dict_log=ts_dict)
+log_np_convert = convert(log_dict)
+
+to_json(path = 'processed_1_log.json',dict_log=log_np_convert)
+
 #dataset format
 '''ds_log = np.zeros(
     (len(ts_dict),2),dtype = np.int32)'''
 # BUG 存在问题 每个区域下的日志长度是不确定的  
+def to_int(x):
+    return int(x)
+md = map(to_int,log_np[:,1]) 
+__time = list(md) # time list
+
+__head = np.min(__time)
+__tail = np.max(__time)
+__length = __tail - __head +1
+action_series = np.zeros((__length,1),dtype='a32')
+
+for row in log_np:
+    __t = int(row[1]) # time now
+    __location = __t - __head
+    action_series[__location] = row[0]
+
+
+
+x = np.array([[b'a',int(10)],  [b'v',3],  [b'c',0]])  
+print (x)
+print ('对 x 调用 argsort() 函数：')
+y = np.argsort(x[:,1])  
+print (y)
+print ('\n')
+print ('以排序后的顺序重构原数组：')
+print (x[y])
+print ('\n')
+print ('使用循环重构原数组：')
+for i in y:  
+    print (x[i], end=" ")
