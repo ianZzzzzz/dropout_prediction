fcc14c3ea39cf70163e06db82e22ea33819ee84b
ianZzzzzz <zhengyh186@gmail.com>
2021-01-16 18:18:22 +0800

    tfidf and n-gram finish

--- analy_pipline.py
+++ analy_pipline.py
@@ -9,7 +9,7 @@
         进入模型
 '''
 from typing import List, Dict
-from numpy import ndarray
+from numpy import ndarray 
 import numpy as np
 
 TEST_OR_NOT = False
@@ -130,6 +130,40 @@ def plot_histogram(log_list:list):
 
         plt.hist( series, bins =  bins_)
         plt.show() '''
+def to_int(Sample:list)->list:
+    Sample = list(map(lambda x: str(x), Sample))
+    return Sample
+
+def count_words(Sample:list)-> list:
+    '''
+        函数功能：根据传入的参数选择相应的方式向量化文本 
+            1 word bag
+            2 n-gram
+            
+    '''
+    # 转换成字符串 因为sklrarn的库只接受str类型的样本
+    
+    from sklearn.feature_extraction.text import CountVectorizer
+    # 实例化
+    vectorizer = CountVectorizer()
+    # 遍历所有样本来建立词表
+    bag = vectorizer.fit(Sample)
+    # 向量化
+    vector = vectorizer.transform(Sample)
+
+    return vector.toarray().tolist()
+
+def tf_idf_width(Sample:list):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    
+    # 实例化
+    vectorizer = TfidfVectorizer()
+    # 标记并建立索引
+    vectorizer.fit(Sample)
+    # 编码文档
+    vector = vectorizer.transform(Sample)
+
+
 
 json_export_path = 'washed_log_list.json'
 
@@ -141,10 +175,64 @@ useful_list = cut_toolong_tooshort(log_list,up = 2000,down = 100)
 # use n-gram can use more useful data
 plot_histogram(useful_list) 
 avg_series_len = find_avg_length_of_series(useful_list)
+Sample = to_int(useful_list)
 
 
+def ngram_vectorize(train_texts, train_labels, val_texts):
+    
+        
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.feature_selection import SelectKBest
+    from sklearn.feature_selection import f_classif
 
+    # Vectorization parameters
+    # Range (inclusive) of n-gram sizes for tokenizing text.
+    NGRAM_RANGE = (1, 2)
 
+    # Limit on the number of features. We use the top 20K features.
+    TOP_K = 20000
 
+    # Whether text should be split into word or character n-grams.
+    # One of 'word', 'char'.
+    TOKEN_MODE = 'word'
+
+    # Minimum document/corpus frequency below which a token will be discarded.
+    MIN_DOCUMENT_FREQUENCY = 2
+    
+    """Vectorizes texts as n-gram vectors.
+
+    1 text = 1 tf-idf vector the length of vocabulary of unigrams + bigrams.
+
+    # Arguments
+        train_texts: list, training text strings.
+        train_labels: np.ndarray, training labels.
+        val_texts: list, validation text strings.
+
+    # Returns
+        x_train, x_val: vectorized training and validation texts
+    """
+    # Create keyword arguments to pass to the 'tf-idf' vectorizer.
+    kwargs = {
+            'ngram_range': NGRAM_RANGE,  # Use 1-grams + 2-grams.
+            'dtype': 'int32',
+            'strip_accents': 'unicode',
+            'decode_error': 'replace',
+            'analyzer': TOKEN_MODE,  # Split text into word tokens.
+            'min_df': MIN_DOCUMENT_FREQUENCY,
+    }
+    vectorizer = TfidfVectorizer(**kwargs)
+
+    # Learn vocabulary from training texts and vectorize training texts.
+    x_train = vectorizer.fit_transform(train_texts)
+
+    # Vectorize validation texts.
+    x_val = vectorizer.transform(val_texts)
+
+    # Select top 'k' of the vectorized features.
+    selector = SelectKBest(f_classif, k=min(TOP_K, x_train.shape[1]))
+    selector.fit(x_train, train_labels)
+    x_train = selector.transform(x_train).astype('float32')
+    x_val = selector.transform(x_val).astype('float32')
+    return x_train, x_val
 
 
