3c4c580f729006fd4204ccb0117e652df4a11b04
ianZzzzzz <zhengyh186@gmail.com>
2021-03-04 22:04:34 +0800

    change series length filter in caculate_time_interval fun

--- Piplines/user_and_course_info_dataset_Pipline.py
+++ Piplines/user_and_course_info_dataset_Pipline.py
@@ -169,7 +169,25 @@ def make_info_dataset(return_mode: str
     ,enroll_find_user
     ,dict_c_info
     ,dict_u_info)->dict or list:
+    """[summary]
 
+    Args:
+        return_mode (str): [description]
+        enroll_find_course ([type]): [description]
+        enroll_find_user ([type]): [description]
+        dict_c_info ([type]): [description]
+        dict_u_info ([type]): [description]
+
+    Returns:
+        dict or list: [info_ = [
+            gender
+            ,birth_year
+            ,edu_degree
+            ,course_category
+            ,course_type
+            ,course_duration
+        ]]
+    """    
 
     dict_enroll_info = {}
     list_enroll_info = []
@@ -274,37 +292,29 @@ dict_enroll_info_full = make_info_dataset(
 #================ info dataset finish ==================#
 dict_enroll_info_full = json.load(open('json_file\\dict_enroll_info_full.json','r'))
 #================ assemble train and test dataset ===============#
-dict_enroll_info = json.load(open('dict_enroll_info.json','r'))
-dict_log_train = json.load(
-    open(
-        'D:\\zyh\\data\\prediction_data\\advance_preprocess_json\\dict_train_log.json'
-        ,'r')
-        ) 
-dict_log_test = json.load(
-    open(
-        'D:\\zyh\\data\\prediction_data\\advance_preprocess_json\\dict_log_test_sec_version.json'
-        ,'r')
-        ) 
 
-dict_log = dict_log_test
 def caculate_time_interval(dict_log)-> dict:
-    pass
+
 
     def static_(interval_list):
-            try:
+            
+            if len(interval_list) > int(3):
                 R = interval_list
                 R_mean = np.mean(R) # 计算均值
                 R_var = np.var(R)   # 计算方差
                 R_sc = np.mean((R - R_mean) ** 3)  #计算偏斜度
                 R_ku = np.mean((R - R_mean) ** 4) / pow(R_var, 2) #计算峰度
 
+                R_sc = np.abs(R_sc)
+                R_ku = np.abs(R_ku)
+
                 static_list = [
-                    int(R_mean)
-                    ,int(R_var)
+                    round(R_mean,2)
+                    ,round(R_var,2)
                     ,round(R_sc,2)
                     ,round(R_ku,2)]
 
-            except:
+            else:
                 static_list = [
                     np.nan,
                     np.nan,
@@ -315,13 +325,13 @@ def caculate_time_interval(dict_log)-> dict:
             
             return static_list
 
-    time_interval_dict = {}
+    time_interval_dict   = {}
     static_interval_dict = {}
-    enroll_scene_dict = {}
+    enroll_scene_dict    = {}
     i = 0
     for e_id,list_log in dict_log.items():
         # 1 time interval
-        long_interval_list = []
+        long_interval_list  = []
         short_interval_list = []
         # 2 state transition
         scene_dict = {}
@@ -339,13 +349,14 @@ def caculate_time_interval(dict_log)-> dict:
             next_action = row_next[1]
             next_object = row_next[2]
             next_session = row_next[3]
-
-            time_interval = int(next_time - now_time)
-            if time_interval >int(60*30): # half hour 
-                long_interval_list.append(time_interval)
-            else:
-                short_interval_list.append(time_interval)
-
+            try:
+                time_interval = int(next_time - now_time)
+                if time_interval >int(60*30): # half hour 
+                    long_interval_list.append(time_interval)
+                else:
+                    short_interval_list.append(time_interval)
+            except:
+                print(next_time,now_time)
         
         short_static = static_(short_interval_list)
         long_static  = static_(long_interval_list)
@@ -359,27 +370,6 @@ def caculate_time_interval(dict_log)-> dict:
         #enroll_scene_dict[int(e_id)] = scene_dict
         # break
     return static_interval_dict
-
-dict_static_intervel_test = caculate_time_interval(dict_log_test)
-i = 0
-for k,v in dict_enroll_info.items():
-    i+=1
-    print(k,v)
-    if i==100:break
-
-list_test_static_info_dataset = []
-list_test_label = []
-for e_id,static_ in dict_static_intervel_test.items():
-    info_ = dict_enroll_info[int(e_id)]
-    info_.extend(static_)
-    assenbled_data = info_
-
-    list_test_static_info_dataset.append(assenbled_data)
-    list_test_label.append(dict_test_label[int(e_id)])
-
-
-
-
 def label_list_to_dict(list_:list)-> dict:
     """[convert list to dict  use the 1st cloumn make index 2nd column make value]
 
@@ -404,6 +394,70 @@ list_test_label = pd.read_csv('prediction_log\\test_truth.csv').values.tolist()
 
 dict_train_label = label_list_to_dict(list_train_label)
 dict_test_label = label_list_to_dict(list_test_label)
+
+dict_log_test = json.load(
+    open(
+        'D:\\zyh\\data\\prediction_data\\advance_preprocess_json\\dict_log_test_sec_version.json'
+        ,'r')
+        ) 
+dict_static_intervel_test = caculate_time_interval(dict_log_test)
+  
+i = 0
+list_test_static_info_dataset = []
+# c_inf0,u_info,long_static,short_static
+list_test_label = []
+for e_id,static_ in dict_static_intervel_test.items():
+    info_ = dict_enroll_info_full[str(e_id)]
+    info_.extend(static_)
+    assenbled_data = info_
+
+    list_test_static_info_dataset.append(assenbled_data)
+    list_test_label.append(dict_test_label[int(e_id)])
+    i+=1
+    if (i%1000)==0:print(i)
+
+json.dump(
+    list_test_label,
+    open('json_file\\into_model\\list_test_label.json','w'))
+json.dump(
+    list_test_static_info_dataset,
+    open('json_file\\into_model\\list_test_static_info_dataset.json','w'))
+
+
+
+dict_log_train = json.load(
+    open(
+        'D:\\zyh\\data\\prediction_data\\advance_preprocess_json\\dict_log_train_sec_version.json'
+        ,'r')
+        ) 
+dict_static_intervel_train = caculate_time_interval(dict_log_train)
+  
+i = 0
+list_train_static_info_dataset = []
+# c_inf0,u_info,long_static,short_static
+list_train_label = []
+for e_id,static_ in dict_static_intervel_train.items():
+    info_ = dict_enroll_info_full[str(e_id)]
+    info_.extend(static_)
+    assenbled_data = info_
+
+    list_train_static_info_dataset.append(assenbled_data)
+    list_train_label.append(dict_train_label[int(e_id)])
+    i+=1
+    if (i%1000)==0:print(i)
+
+json.dump(
+    list_train_label,
+    open('json_file\\into_model\\list_train_label.json','w'))
+json.dump(
+    list_train_static_info_dataset,
+    open('json_file\\into_model\\list_train_static_info_dataset.json','w'))
+
+
+
+
+
+
 
 for e_id , label in dict_train_label.items():
     list_info = dict_enroll_info[int(e_id)]
